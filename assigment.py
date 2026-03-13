665562f33a42a08b682e2b9e6e00a659417fc3fd
peace <peacemwende899@gmail.com>
2026-03-13 22:30:44 +0300

    Update assignment.py

--- assigment.py
+++ assigment.py
@@ -1,1 +1,26 @@
+# QUESTION 4
+print("Hello AI Students")
 
+# QUESTION 5
+Name= "John"
+Age= 20
+City= "Nairobi"
+print("Name:",Name)
+#Name: John
+print("Age:",Age)
+#Age: 20
+print("City:",City)
+#City: Nairobi
+
+# QUESTION 6
+languages= ["Python","Java","C++","JavaScript","Go"]
+print(languages[0])
+#Output- python
+# QUESTION 7
+student= {
+  "Name":"Peace",
+  "Age":29,
+  "Course":"AI"
+}
+print(student["Name"])
+#Output- Peace
